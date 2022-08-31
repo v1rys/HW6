5fb0d0e7324818ba2d95b22aa34c9139cb24eb5c
v1rys <alaverdi@gmail.com>
2022-09-01 04:03:33 +0700

    Finish HomeWork6

---
+++ Task 43/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите число b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+b1 = Math.Abs(b1);
+Console.WriteLine("Введите число k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+k1 = Math.Abs(k1);
+Console.WriteLine("Введите число b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+b2 = Math.Abs(b2);
+Console.WriteLine("Введите число k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+k2 = Math.Abs(k2);
+
+
+if (k1==k2  &&  b1==b2)
+{
+    Console.WriteLine("Прямые прямые совпадают");
+}
+else if ( k1 == k2)
+{
+    Console.WriteLine("прямые прямые паралелны");
+}
+
+double x = 0;
+double y = 0;
+
+x = (b2-b1)/(k1-k2);
+y = (k1*(b2-b1))/(k1-k2)+b1;
+Console.WriteLine($"Кординаты точек {x} , {y}");
+
+// я не понимаю как выводить дробное значение 
+//у меня нули выводяться кода значение должно быть -0.5
+
+
+
+
---
+++ Task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите число М оно будет показывать сколько чисел вам нужно бут ввести");
+int M = Convert.ToInt32(Console.ReadLine());
+int CounterMZero = CounterForMoreZero(M);
+Console.WriteLine($"Чисел больше нуля {CounterMZero}");
+
+
+int CounterForMoreZero(int num)
+{
+    int [] arr = new int [M];
+    int counter = 0;
+    for(int i = 0; i < arr.Length; i++ )
+    {
+        Console.WriteLine("Введите число X");
+        int X = Convert.ToInt32(Console.ReadLine());
+        arr[i] = X;
+        if ( arr[i] > 0)
+        {
+           counter++; 
+        }
+    }
+    return counter;
+}
